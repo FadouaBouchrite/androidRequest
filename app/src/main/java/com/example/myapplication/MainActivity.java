f66778679df050b04b28d8dc3c542132113ef729
fadoua <fadouabouch98@gmail.com>
2023-12-18 18:29:33 +0100

    envoyer une image

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,37 +1,81 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+import java.io.BufferedInputStream;
 import java.io.IOException;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String SERVER_URL = "http://192.168.43.112:5000/upload"; // Change this to your Flask server URL
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        OkHttpClient okHttpClient = new OkHttpClient();
-        Request request = new Request.Builder().url("http://192.168.43.112:5000/test").build();
-
-        okHttpClient.newCall(request).enqueue(new Callback() {
+        Button uploadButton = findViewById(R.id.uploadButton);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                String errorMessage = e.getMessage();
-                runOnUiThread(() -> Toast.makeText(getApplicationContext(), "Error: " + errorMessage, Toast.LENGTH_LONG).show());
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                runOnUiThread(() -> Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show());
+            public void onClick(View v) {
+                new UploadImageTask().execute("https://via.placeholder.com/150");
             }
         });
+    }
+
+    private class UploadImageTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String imageUrl = urls[0];
+
+            try {
+                URL url = new URL(SERVER_URL);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+
+                InputStream inputStream = new BufferedInputStream(new URL(imageUrl).openStream());
+                byte[] buffer = new byte[8192];
+                int bytesRead;
+
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    urlConnection.getOutputStream().write(buffer, 0, bytesRead);
+                }
+
+                inputStream.close();
+                urlConnection.getOutputStream().close();
+
+                int responseCode = urlConnection.getResponseCode();
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    return "Image uploaded successfully";
+                } else {
+                    return "Error uploading image. Response code: " + responseCode;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "Error: " + e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+        }
     }
 }
